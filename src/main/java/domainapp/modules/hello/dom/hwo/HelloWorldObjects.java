322c13200df3aee28958b382c9b7531121c5aea3
danhaywood <dan@haywood-associates.co.uk>
2021-07-04 17:24:38 +0100

    updates for ImpersonateMenu and UserMenu

--- src/main/java/domainapp/modules/hello/dom/hwo/HelloWorldObjects.java
+++ src/main/java/domainapp/modules/hello/dom/hwo/HelloWorldObjects.java
@@ -8,6 +8,7 @@ import org.apache.isis.applib.annotation.Action;
 import org.apache.isis.applib.annotation.ActionLayout;
 import org.apache.isis.applib.annotation.DomainService;
 import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.PriorityPrecedence;
 import org.apache.isis.applib.annotation.PromptStyle;
 import org.apache.isis.applib.annotation.RestrictTo;
 import org.apache.isis.applib.annotation.SemanticsOf;
@@ -18,7 +19,8 @@ import domainapp.modules.hello.types.Name;
 @DomainService(
         nature = NatureOfService.VIEW,
         objectType = "hello.HelloWorldObjects"
-        )
+)
+@javax.annotation.Priority(PriorityPrecedence.EARLY)
 public class HelloWorldObjects {
 
     private final RepositoryService repositoryService;
